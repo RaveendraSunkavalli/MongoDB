8e451baccd6f64cd7f3b2683c3670e70811afe9a
Ravi <s.ravindra153@gmail.com>
2017-12-28 23:20:52 +0530

    Fetching all data from Todos

---
+++ Playground/mongodb-find.js
@@ -0,0 +1,34 @@
+const {MongoClient,ObjectID}=require("mongodb");
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp',(err,db)=>{
+    if(err){
+        return console.log("unable to connect to MongoDB server");
+    }
+    console.log('Connected to MongoDB server');
+    const myDB=db.db("Todos");
+    myDB.collection("Todos").find().toArray().then((docs)=>{
+         console.log("Todos");
+        console.log(JSON.stringify(docs,"",4));
+   },(err)=>{
+        console.log(err);
+    });
+    myDB.collection("Todos").find({completed:false}).toArray().then((docs)=>{
+        console.log("Todos");
+        console.log(JSON.stringify(docs,"",4));
+    },(err)=>{
+        console.log(err);
+    });
+
+  
+    db.close();
+});
+
+
+
+
+
+
+
+
+
+    
