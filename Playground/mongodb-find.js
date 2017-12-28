2031b341996eb180fa99d0612f79458a40355cfb
Ravi <s.ravindra153@gmail.com>
2017-12-28 23:22:00 +0530

    Fetching particalar data from Todos

--- Playground/mongodb-find.js
+++ Playground/mongodb-find.js
@@ -19,7 +19,15 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp',(err,db)=>{
         console.log(err);
     });
 
-  
+    myDB.collection("Todos").find({
+        _id:new ObjectID('5a44ed5320823811800eacd0'),
+    }
+    ).toArray().then((docs)=>{
+        console.log("Todos");
+        console.log(JSON.stringify(docs,"",4));
+    },(err)=>{
+        console.log(err);
+    });
     db.close();
 });
 
