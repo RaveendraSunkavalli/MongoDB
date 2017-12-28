0b03f93b1f33cb033c27db483c5a06c61c54f43c
Ravi <s.ravindra153@gmail.com>
2017-12-28 23:14:06 +0530

    Inserting document to Todos

--- Playground/mongodb-connect.js
+++ Playground/mongodb-connect.js
@@ -7,6 +7,16 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp',(err,db)=>{
         return console.log("unable to connect to MongoDB server");
     }
     console.log('Connected to MongoDB server');
-    
+    const myDB=db.db("Todos");
+    myDB.collection("Todos").insertOne({
+        text:'Walk the dog',
+         completed:false,
+   },(err,res)=>{
+        if(err){
+            return console.log("unable to create",err);
+        }
+        console.log(JSON.stringify(res.ops));
+   });
+
     db.close();
 });
