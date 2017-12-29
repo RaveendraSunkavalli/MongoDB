442e1dc87ff2808f7a14f2fdfadac99ee1621ae7
Ravi <s.ravindra153@gmail.com>
2017-12-29 11:43:03 +0530

    Delete Document using deleteMany()

---
+++ Playground/mongodb-delete.js
@@ -0,0 +1,15 @@
+const {MongoClient,ObjectID}=require("mongodb");
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp',(err,db)=>{
+    if(err){
+        return console.log("unable to connect to MongoDB server");
+    }
+    console.log('Connected to MongoDB server');
+    const myDB=db.db("Todos");
+    myDB.collection('Todos').deleteMany({text:'Walk the dog'}).then((res)=>{
+        console.log(res);
+    },(err)=>{
+        console.log(err);
+    });
+    db.close();
+});
