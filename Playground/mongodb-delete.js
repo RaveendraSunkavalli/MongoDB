cbb8c9bc2cc4315803ce000f53aef9c3734fdc8a
Ravi <s.ravindra153@gmail.com>
2017-12-29 11:46:37 +0530

    Deleting Document using deleteOne()

--- Playground/mongodb-delete.js
+++ Playground/mongodb-delete.js
@@ -6,7 +6,12 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp',(err,db)=>{
     }
     console.log('Connected to MongoDB server');
     const myDB=db.db("Todos");
-    myDB.collection('Todos').deleteMany({text:'Walk the dog'}).then((res)=>{
+    // myDB.collection('Todos').deleteMany({text:'Walk the dog'}).then((res)=>{
+    //     console.log(res);
+    // },(err)=>{
+    //     console.log(err);
+    // });
+    myDB.collection('Todos').deleteOne({text:'Walk the dog'}).then((res)=>{
         console.log(res);
     },(err)=>{
         console.log(err);
