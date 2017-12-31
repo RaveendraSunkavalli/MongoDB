8138499a3d8bc348d63cada8e72c7874acf740ce
Ravi <s.ravindra153@gmail.com>
2018-01-01 00:03:43 +0530

    getting users info

--- Playground/mongodb-querie.js
+++ Playground/mongodb-querie.js
@@ -5,12 +5,13 @@ const {
     Todo
 } = require('../Server/Models/Todos');
 const {
-    user
+    User
 } = require('../Server/Models/Users');
 const {
     ObjectID
 } = require('mongodb');
 var id = "5a4925c84670da1f48d0abb5";
+var userid="5a49241391e674052ce3edc1";
 if (!ObjectID.isValid(id)) {
     console.log("ID not a Valid");
 }
@@ -40,4 +41,35 @@ Todo.findById(id).then(doc => {
         console.log("todoById:", doc);
 }, err => {
     console.log(err);
+});
+if (!ObjectID.isValid(userid)) {
+    console.log("ID not a Valid");
+}
+User.findById(userid).then(doc=>{
+    if(!doc)
+        console.log("Id not Found");
+    else
+        console.log("user:",doc)
+});
+
+User.find({_id:userid}).then(doc=>{
+    if(!doc)
+        console.log("Id not Found");
+    else
+        console.log("user:",doc)
+});
+
+User.findOne({_id:userid}).then(doc=>{
+    if(!doc)
+        console.log("Id not Found");
+    else
+        console.log("user:",doc)
+});
+
+
+User.findById(userid).then(doc=>{
+    if(!doc)
+        console.log("Id not Found");
+    else
+        console.log("user:",doc)
 });
