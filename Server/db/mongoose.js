c5187e27cbeb978cd4e96e5b24660103461f63f4
Ravi <s.ravindra153@gmail.com>
2018-01-01 14:23:12 +0530

    modified monogoose.js with url

--- Server/db/mongoose.js
+++ Server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose=require('mongoose');
 mongoose.Promise=global.Promise;
 
-mongoose.connect('mongodb://localhost:27017/TodoApp').then(()=>{
+mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds237967.mlab.com:37967/ravindranodepractice' || 'mongodb://localhost:27017/TodoApp').then(()=>{
     console.log("connected to mongodb");
 },(err)=>{
     console.log(err);
