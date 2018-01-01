f541a8efb3e8ba3b6cd850b23aa93c3e6788c42b
Ravi <s.ravindra153@gmail.com>
2018-01-01 14:00:54 +0530

    Deploying to heroku

--- Server/server.js
+++ Server/server.js
@@ -1,63 +1,75 @@
+const express = require('express');
+const bodyParser = require("body-parser");
+const {
+    ObjectID
+} = require('mongodb');
 
-const express=require('express');
-const bodyParser=require("body-parser");
-const {ObjectID}=require('mongodb');
+
+var {
+    mongoose
+} = require("./db/mongoose");
+var {
+    Todo
+} = require('./Models/Todos');
+var {
+    User
+} = require('./Models/Users');
+var app = express();
 
 
-var {mongoose}=require("./db/mongoose");
-var {Todo}=require('./Models/Todos');
-var {User}=require('./Models/Users');
-var app=express();
+var port=process.env.PORT|| 3000;
+
 app.use(bodyParser.json());
-app.post('/todos',(req,res)=>{
-    var todo=new Todo({
-        text:req.body.text
+app.post('/todos', (req, res) => {
+    var todo = new Todo({
+        text: req.body.text
     });
-    todo.save().then((doc)=>{
+    todo.save().then((doc) => {
         res.send(doc);
-    },(err)=>{
+    }, (err) => {
         res.status(400).send(err);
 
     });
 });
 
-app.post('/user',(req,res)=>{
-    
-    var usr=new User({
-        email:req.body.email
+app.post('/user', (req, res) => {
+
+    var usr = new User({
+        email: req.body.email
     });
-    usr.save().then((doc)=>{
+    usr.save().then((doc) => {
         res.send(doc);
-    },(err)=>{
+    }, (err) => {
         res.status(400).send(err);
     });
 });
-app.get('/todos',(req,res)=>{
-    Todo.find().then((doc)=>{
+app.get('/todos', (req, res) => {
+    Todo.find().then((doc) => {
         res.send(doc);
-    },err=>{
+    }, err => {
         res.status(400).send(err);
     });
 });
 
-app.get('/todos/:id',(req,res)=>{
-    var id=req.params.id;
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
     ObjectID.isValid(id)
-    if(ObjectID.isValid(id)){
-        Todo.findById(id).then((doc)=>{
-            if(!doc){
+    if (ObjectID.isValid(id)) {
+        Todo.findById(id).then((doc) => {
+            if (!doc) {
                 res.status(400).send();
-            }else
+            } else
                 res.send(doc);
-        }),(err=>{
+        }), (err => {
             res.status(400).send(err);
         });
-    }else
+    } else
         res.status(404).send();
 });
 
-app.listen(3000,()=>{
+app.listen(port, () => {
     console.log("connected to localhost");
 });
- module.exports={app};
-
+module.exports = {
+    app
+};
