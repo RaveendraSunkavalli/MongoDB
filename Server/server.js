9f2b737b296646016e07707f10c17a23a6830d13
Ravi <s.ravindra153@gmail.com>
2018-01-10 17:29:05 +0530

    adding missing get route and update timeout in package.json

--- Server/server.js
+++ Server/server.js
@@ -58,6 +58,16 @@ app.post('/todos', (req, res) => {
 
     });
 });
+app.get('/todos', (req, res) => {
+    Todo.find().then((doc) => {
+            if (!doc) {
+                res.status(400).send();
+            } else
+                res.send(doc);
+        }), (err => {
+            res.status(404).send(err);
+        });
+});
 
 app.get('/todos/:id', (req, res) => {
     var id = req.params.id;
